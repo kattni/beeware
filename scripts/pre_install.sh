adfd72a1e833f4c8f8d1e03170eff0c8d6db554b
Kattni <kattni@kattni.com>
2025-07-18 20:46:16 -0400

    Build dependency.

--- scripts/pre_install.sh
+++ scripts/pre_install.sh
@@ -1,1 +1,8 @@
-sudo apt-get update && apt-get install -y libmd4c-dev
+git clone https://github.com/mity/md4c.git --depth 1
+cd md4c
+cmake -B build
+cmake --build build
+cd build
+sudo cmake --install .
+cd ../..
+rm -rf md4c
